877dd58de22007509ef19f4b30e4a85724201c9e
LefkaLefka <lefka.velchenko@gmail.com>
2017-01-24 22:20:02 +0200

    Test main type script file

---
+++ src/ts/main.ts
@@ -0,0 +1,15 @@
+// var el = $("[data-content]");
+//
+// class User{
+//     name : string;
+//     age : number;
+//     constructor(_name:string, _age: number){
+//
+//         this.name = _name;
+//         this.age = _age;
+//     }
+// }
+//
+// var tom : User = new User("Том", 29);
+//
+// el.append("Имя: " + tom.name + " возраст: " + tom.age);
