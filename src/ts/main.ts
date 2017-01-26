332b582b451b0e01eaddb20189855bde2f53dfda
LefkaLefka <lefka.velchenko@gmail.com>
2017-01-26 10:02:31 +0200

    Test

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,15 +1,5 @@
-// var el = $("[data-content]");
-//
-// class User{
-//     name : string;
-//     age : number;
-//     constructor(_name:string, _age: number){
-//
-//         this.name = _name;
-//         this.age = _age;
-//     }
-// }
-//
-// var tom : User = new User("Том", 29);
-//
-// el.append("Имя: " + tom.name + " возраст: " + tom.age);
+import { Movement } from "../ts/modules/movement"
+import { Game } from "../ts/modules/game"
+
+let game : Game = new Game();
+new Movement(game);
