60555b25e9eb666c9232c8c27fb3975152d43881
LefkaLefka <lefka.velchenko@gmail.com>
2017-01-26 10:30:46 +0200

    Added interface of player score. Some comments

--- src/ts/modules/game.ts
+++ src/ts/modules/game.ts
@@ -1,18 +1,34 @@
+/**
+ * Interface for set players score.
+ */
+interface Score {
+    player1 : number;
+    player2 : number;
+}
+
 export class Game {
     field : number[][];
     sizeField : number = 3;
-    score = {
-        player1 : number = 0,
-        player2 : number = 0
-    };
+    score : Score;
+    /**
+     * Constructor of game, where set main array and players score.
+     */
     constructor() {
-        let field : number[][] = [[-1, -1, -1],
-                                  [-1, -1, -1],
-                                  [-1, -1, -1]];
-        this.field = field;
+        this.field = [[-1, -1, -1],
+                      [-1, -1, -1],
+                      [-1, -1, -1]];
+        this.score = {
+            player1 : 0,
+            player2 : 0
+        };
     }
+    /**
+     * Function to handle the move player.
+     * @param i Coordinates for x.
+     * @param j Coordinates for y.
+     * @returns {any} Boolean : false - when cell is busy. String : "o" | "x" - when cell is free.
+     */
     move(i : number, j : number) : boolean | string {
-        // this.score.player1++;
         if(this.field[i][j] === -1) {
             // Выбор игрока
             this.field[i][j] = 1;
